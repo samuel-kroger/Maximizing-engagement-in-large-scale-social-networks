5199920da7f86726568dde06ab1c1815c3943f06
samuel-kroger <55402608+samuel-kroger@users.noreply.github.com>
2022-04-02 00:07:35 -0500

    Heurisitic time

--- code/RCM_OLAK_results.py
+++ code/RCM_OLAK_results.py
@@ -5,6 +5,7 @@ import time
 import json
 from datetime import datetime
 import networkx as nx
+import os
 
 ext = "../data/"
 dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M")
@@ -14,35 +15,48 @@ filename = filename.strip()
 f = open('data.json')
 data = json.load(f)
 
-for request in data['single']:
+for request in data['heuristic']:
 	print("starting: ",
 		'\n filename: ', request['filename'],
-		'\n model_type: ', request['model_type'],
 		'\n k: ', request['k'],
 		'\n b: ', request['b'])
 
 	G = classes.read_graph(ext + request['filename'])
 	k = request['k']
 	b = request['b']
-	start_time = time.time()
+
+	k_core = list(nx.k_core(G, k))
+	print(len(k_core))
+
+	rcm_time_start = time.time()
 	r = rcm.RCM(G, k, b)
 	a, f = r.findAnchors()
+	rcm_k_core = classes.anchored_k_core(G, k, a)
+	print(len(rcm_k_core))
+	rcm_time_end = time.time()
 
-	k_core = classes.anchored_k_core(G, k, [])
-	k_core2 = nx.k_core(G, k)
-	print('test1')
-	print(len(k_core))
-	print(len(k_core2))
+	olak_time_start = time.time()
+	olak_output = list(olak.olakAnchors(G, olak.anchoredKCore(G), k, b))
+	olak_anchors = [element for element in olak_output if element not in k_core]
+	olak_k_core = classes.anchored_k_core(G, k, olak_anchors)
+	olak_time_end = time.time()
 
-	rcm_core = classes.anchored_k_core(G, k, a)
-	print('test2')
-	print(len(a))
-	print(len(f))
+	print(len(olak_k_core))
 
-	print('test3')
-	print(len(f) + len(k_core))
-	print(len(rcm_core) - len(k_core))
+	title = ['instance', 'k', 'b', 'olak', 'olak time', 'rcm', 'rcmtime']
+	results = [request['filename'], str(k), str(b), str(len(rcm_k_core)), str(round(rcm_time_end - rcm_time_start, 2)), str(len(olak_k_core)), str(round(olak_time_end - olak_time_start, 2))]
+	if not os.path.exists("../results/" + filename):
+			with open("../results/" + filename, "w") as doc:
+				string = ""
+				for thing in title:
+					string += thing + ", "
+				string += 'total_time'
+				doc.write(string)
+				doc.close()
 
-
-
-	end_time = time.time()
+	with open("../results/" + filename, "a") as doc:
+		string = "\n"
+		for thing in results:
+			string += thing + ", "
+		doc.write(string)
+		doc.close()
