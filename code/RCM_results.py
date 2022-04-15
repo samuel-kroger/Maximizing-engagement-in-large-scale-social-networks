e16754ca030fda7f6abfe04e217615500bcedf6f
samuel-kroger <55402608+samuel-kroger@users.noreply.github.com>
2022-04-15 11:35:38 -0500

    Update RCM_results.py

--- code/RCM_results.py
+++ code/RCM_results.py
@@ -26,23 +26,6 @@ for request in data['below_the_line']:
 	k = request['k']
 	b = request['b']
 
-	#k_core = nx.k_core(G, k)
-
-	#k_core_decomposition = olak.anchoredKCore(G)
-	#tracker = []
-	#for node in k_core_decomposition:
-	#	tracker.append(k_core_decomposition[node])
-
-	#print("median k: " + str(median(tracker)))
-	#print("cardinality of med k_core: " + str((len(k_core))))
-
-	k_core = list(nx.k_core(G, k))
-	#print(len(G.nodes()))
-	#print(len(G.edges()))
-	#print(len(k_core))
-
-
-
 
 	rcm_time_start = time.time()
 	r = rcm.RCM(G, k, b)
@@ -50,17 +33,6 @@ for request in data['below_the_line']:
 	rcm_k_core = classes.anchored_k_core(G, k, a)
 	print(len(rcm_k_core))
 	rcm_time_end = time.time()
-	'''
-	olak_time_start = time.time()
-	olak_output = list(olak.olakAnchors(G, olak.anchoredKCore(G), k, b))
-	olak_anchors = [element for element in olak_output if element not in k_core]
-	olak_k_core = classes.anchored_k_core(G, k, olak_anchors)
-	olak_time_end = time.time()
-	'''
-	print(len(olak_k_core))
-
-	#title = ['instance', 'k', 'b', 'olak', 'olak time', 'rcm', 'rcmtime']
-	#results = [request['filename'], str(k), str(b), str(len(rcm_k_core)), str(round(rcm_time_end - rcm_time_start, 2)), str(len(olak_k_core)), str(round(olak_time_end - olak_time_start, 2))]
 
 	title = ['instance', 'k', 'b', 'rcm', 'rcmtime']
 	results = [request['filename'], str(k), str(b), str(len(rcm_k_core)), str(round(rcm_time_end - rcm_time_start, 2))]
