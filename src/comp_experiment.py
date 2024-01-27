b5d51e9825580acf5829184cd84ae998ddefc04f
samuel-kroger <sak8@rice.edu>
2024-01-27 09:24:35 -0600

    new

--- src/comp_experiment.py
+++ src/comp_experiment.py
@@ -13,7 +13,7 @@ filename = filename.strip()
 f = open('data.json')
 data = json.load(f)
 
-for request in data['table_2']:
+for request in data['table_2_current']:
 	print("starting: ",
 		'\n filename: ', request['filename'],
 		'\n model_type: ', request['model_type'],
